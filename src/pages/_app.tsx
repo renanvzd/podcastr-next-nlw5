3d136a03a818a91c84ff2f3a4fa4681af6695ce4
Renan Veronez Drechsler <renanvz@outlook.com>
2021-04-22 20:29:15 -0300

    Contexts - PlayerContext.ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
+import '../styles/global.scss'
+
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
+import { PlayerContext } from '../contexts/PlayerContext';
 
-import '../styles/global.scss'
 import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <PlayerContext.Provider value={'Renan'}>
     <div className={styles.wrapper}>
       <main>
       <Header />
@@ -13,6 +16,7 @@ function MyApp({ Component, pageProps }) {
       </main>
       <Player />
     </div>
+    </PlayerContext.Provider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -26,10 +28,11 @@ type HomeProps = {
 }
  
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const player = useContext(PlayerContext)
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
-        <h2>Últimos Lançamentos</h2>
+        <h2>Últimos Lançamentos {player}</h2>
 
         <ul>
           {latestEpisodes.map(episode => {
