6d121ceb0a466a09ed3d7703de82b8a6f62f8398
Renan Veronez Drechsler <renanvz@outlook.com>
2021-04-21 23:39:31 -0300

    Episode - Description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,6 @@ type Episode = {
   publishedAt: string,
   duration: number,
   durationAsString: string,
-  description: string,
   url: string,
 }
 
@@ -129,9 +128,8 @@ export const getStaticProps: GetStaticProps = async () => {
         thumbnail: episode.thumbnail,
         members: episode.members,
         publishedAt: format(parseISO(episode.published_at), 'd MMM yyy', { locale: ptBR}),
-        duration: episode.file.duration,
+        duration: Number(episode.file.duration),
         durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-        description: episode.description,
         url: episode.file.url,
       };
     })
