75789ce2dff35020eec5535d50cedcc3996c93f2
Renan Veronez Drechsler <renanvz@outlook.com>
2021-04-23 17:54:34 -0300

    Estilizaçao Botoes Player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -54,19 +54,19 @@ export function Player() {
           </div>
 
           <div className={styles.buttons}>
-            <button type="button">
+            <button type="button" disabled={!episode}>
               <img src="/shuffle.svg" alt="Embaralhar" />
             </button>
-            <button type="button">
+            <button type="button" disabled={!episode}>
               <img src="/play-previous.svg" alt="Tocar Anterior" />
             </button>
-            <button type="button" className={styles.playButton}>
+            <button type="button" className={styles.playButton} disabled={!episode}>
               <img src="/play.svg" alt="Tocar" />
             </button>
-            <button type="button">
+            <button type="button" disabled={!episode}>
               <img src="/play-next.svg" alt="Tocar Próxima" />
             </button>
-            <button type="button">
+            <button type="button" disabled={!episode}>
               <img src="/repeat.svg" alt="Repetir" />
             </button>
           </div>
